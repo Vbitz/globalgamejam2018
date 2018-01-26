4a1a4b0ecccf3d6e405604621c572902d0d61c38
jscarsbrook <jscarsbrook@gmail.com>
2018-01-26 18:24:15 +1300

    Commit on 2018-01-26 18:24:15.638846

--- src/app.ts
+++ src/app.ts
@@ -17,15 +17,36 @@ class GlobalGameJamGame {
 
     this.renderer = new THREE.WebGLRenderer({antialias: true});
 
-    this.renderer.setClearColor(new THREE.Color('cornflowerBlue'));
+    this.renderer.setClearColor(new THREE.Color(0x6495ED));
 
-    this.scene = new THREE.Scene;
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    this.scene = new THREE.Scene();
+
+    this.camera = new THREE.PerspectiveCamera();
+
+    this.camera.position.set(10, 10, 10);
+    this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    const cube = new THREE.Mesh(
+        new THREE.CubeGeometry(1, 2, 1),
+        new THREE.MeshLambertMaterial({color: new THREE.Color(0x101010)}));
+
+    this.scene.add(cube);
+
+    const light = new THREE.AmbientLight(0xeaeaea);
+
+    this.scene.add(light);
+
+    this.container.appendChild(this.renderer.domElement);
 
     this.update();
   }
 
   private update() {
     this.renderer.render(this.scene, this.camera);
+
+    requestAnimationFrame(this.update.bind(this));
   }
 }
 
