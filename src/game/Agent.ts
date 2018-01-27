a9036f9e30df6bf876a0e88c5f86ba609e9b29d4
jscarsbrook <jscarsbrook@gmail.com>
2018-01-27 13:04:33 +1300

    Commit on 2018-01-27 13:04:33.635453

--- src/game/Agent.ts
+++ src/game/Agent.ts
@@ -1,5 +1,24 @@
+enum AgentState {
+  /**
+   * Waiting for some kind of command from the player.
+   */
+  Resting,
+
+  /**
+   * Just walking though the dungeon gradually producing a map of the interior.
+   */
+  Adventuring,
+
+  /**
+   * Just encountered something really scarry. Backtracking though steps.
+   */
+  NOPE,
+}
+
 export class Agent {
   private alive = true;
+
+  private state: AgentState = AgentState.Resting;
 
   isAlive() {
     return this.alive;
